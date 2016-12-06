9398e92b08e34ee9f69799917c442aa7c115514e
libaitaohong <492563822@qq.com>
2016-12-06 16:42:05 +0800

    80. Remove Duplicates from Sorted Array II

--- src/RemoveDuplicate2.java
+++ src/RemoveDuplicate2.java
@@ -9,22 +9,19 @@
  * It doesn't matter what you leave beyond the new length.
  * 版本:1
  * 运行时间:Runtime: 1 ms。Your runtime beats 31.85% of java submissions.
- * 备注:不再用sign记录有几个重复值，而使用k来管理下标
+ * 备注:i为有效值当前下标，j遍历数组
+ * nums[i]不是有效值，nums[i-1]才是最后添加的有效值
+ * 因为可以最多放两个一样的，所以前两个肯定有效，所以条件i<2时有效
+ * 当前遍历的值nums[j]，比有效值的倒数第二个就是nums[i-2]大的话，说明没有连续三个一样，
+ * 这时不管是不是当前值与nums[i-1]相同，都把当前值插入到有效值中。
  */
 public class RemoveDuplicate2 {
     public int removeDuplicates(int[] nums) {
-        if(nums.length == 0 || nums.length == 1) return nums.length;
-        int k = 1;
-        int count = 0;
-        for(int i = 1; i < nums.length; i++){
-            if(nums[i-1] == nums[i]){
-                count += 1;
-            }else{
-                count = 0;
-            }
-            if(count <= 1)
-                nums[k++] = nums[i];
+        int i = 0;
+        for(int j = 0; j < nums.length; j++){
+            if(i < 2 || nums[j] > nums[i-2])
+                nums[i++] = nums[j];
         }
-        return k;
+        return i;
     }
 }
