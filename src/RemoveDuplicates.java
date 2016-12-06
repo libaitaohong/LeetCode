8abba9cb8db41d7a42cdf5846f594dce753ec929
libaitaohong <492563822@qq.com>
2016-12-06 11:32:31 +0800

    26 Remove Duplicates from Sorted Array

--- src/RemoveDuplicates.java
+++ src/RemoveDuplicates.java
@@ -10,20 +10,23 @@
  * with the first two elements of nums being 1 and 2 respectively.
  * It doesn't matter what you leave beyond the new length.
  * 版本:
- * Runtime: 13 ms
+ * Runtime: 14 ms. Your runtime beats 55.48% of java submissions.
  * 备注:删除排序数组里的重复数字
+ * 不再用sign记录有几个重复值，而使用k来管理下标。当前值与下一个值比较，不等则添加新值
+ * 最后将数组最后一个值添加进去，因为循环时没有便利到最后一个值，而无论前面有没有重复，最后一个值
+ * 都是要加进数组的。
+ * 表示下标，所以最后返回k+1代表数组中有效值的个数
  */
 public class RemoveDuplicates {
     public int removeDuplicates(int[] nums) {
         if(nums.length == 0 || nums.length == 1) return nums.length;
-        int sign = 0;
-        for(int i = 1; i < nums.length; i++){
-            if(nums[i-1] == nums[i]){
-                sign += 1;
-            }else if(sign != 0){
-                nums[i-sign] = nums[i];
+        int k = 0;
+        for(int i = 0; i < nums.length - 1; i++){
+            if(nums[i] != nums[i+1]){
+                nums[k++] = nums[i];
             }
         }
-        return nums.length-sign;
+        nums[k] = nums[nums.length-1];
+        return k+1;
     }
 }
