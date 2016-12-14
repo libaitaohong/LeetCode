716b49962d86283ee384fb3d11e01efee37a7f75
libaitaohong <492563822@qq.com>
2016-12-14 19:59:42 +0800

    102. Binary Tree Level Order Traversal

--- src/LevelOrder.java
+++ src/LevelOrder.java
@@ -7,36 +7,29 @@ import java.util.List;
  * 时间: 2016/12/14 15:22
  * 题目:102. Binary Tree Level Order Traversal
  * 内容:Given a binary tree, return the level order traversal of its nodes' values.
- * 版本:1
+ * 版本:2
  * 运行时间:
  * 备注:按行打印，按行输出
- * 左程云讲的版本
+ * 每次遍历一层，不需要记录每层最后一个节点。但是时间复杂度一样的
  */
 public class LevelOrder {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> allNode = new LinkedList<>();
-        List<Integer> lineNode = new LinkedList<>();
         LinkedList<TreeNode> order = new LinkedList<>();
         if(root == null) return allNode;
-        TreeNode lineLast = root;
-        TreeNode curLast = root;
         order.add(root);
         while(order.size() != 0){
-            TreeNode cur = order.remove();
-            lineNode.add(cur.val);
-            if(cur.left != null){
-                order.add(cur.left);
-                curLast = cur.left;
+            List<Integer> lineNode = new LinkedList<>();
+            int size = order.size();
+            for(int i = 0; i < size; i++){
+                TreeNode cur = order.remove();
+                lineNode.add(cur.val);
+                if(cur.left != null)
+                    order.add(cur.left);
+                if(cur.right != null)
+                    order.add(cur.right);
             }
-            if(cur.right != null){
-                order.add(cur.right);
-                curLast = cur.right;
-            }
-            if(cur == lineLast){
-                allNode.add(new LinkedList<>(lineNode));
-                lineNode.clear();
-                lineLast = curLast;
-            }
+            allNode.add(lineNode);
         }
         return allNode;
     }
