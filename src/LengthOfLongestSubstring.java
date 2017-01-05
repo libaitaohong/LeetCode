6407442d5a8e24039256a728ddb8a7073fe86796
libaitaohong <492563822@qq.com>
2017-01-05 18:19:40 +0800

    3. Longest Substring Without Repeating Characters

--- src/LengthOfLongestSubstring.java
+++ src/LengthOfLongestSubstring.java
@@ -7,26 +7,29 @@ import java.util.LinkedList;
  * 时间: 2016/12/30 16:29
  * 题目:3. Longest Substring Without Repeating Characters
  * 内容:求一个串的最长无重复子串
- * 版本:1
+ * 版本:2
  * 运行时间:
- * 备注:使用set。
+ * 备注:建立一个数组长度256，用来存放所有字符
+ * 首先设置开始为start=-1，数组里面的值也全是-1
+ * 便利字符串，将字符串在数组中对应的位置设置为字符串下标
+ * 如果发现数组中对应位置的字符串下标比start大，说明目前维护的字符串已经出现了该字符了
+ * 所以让start等于这个字符串的位置，这时新的字符串的第一字符为start的下一个位置字符
+ * 就相当于把重复字符去掉了，在重复字符的下一个字符处重新计算最长子串
  */
 public class LengthOfLongestSubstring {
     public int lengthOfLongestSubstring(String s) {
         if(s == null) return 0;
         int max = 0;
-        int i = 0;
-        int j = 0;
-        HashSet<Character> set = new HashSet<>();
-        while(j < s.length()){
-            if(!set.contains(s.charAt(j))){
-                set.add(s.charAt(j++));
-                max = Math.max(max, set.size());
-            }else{
-                set.remove(s.charAt(i++));
-            }
+        int start = -1;
+        int[] ss = new int[256];
+        for(int i = 0; i < 256; i++)
+            ss[i] = -1;
+        for(int i = 0; i < s.length(); i++){
+            if(ss[s.charAt(i)] > start)
+                start = ss[s.charAt(i)];
+            ss[s.charAt(i)] = i;
+            max = Math.max(max, i-start);
         }
         return max;
-
     }
 }
